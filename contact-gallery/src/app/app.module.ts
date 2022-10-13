d8c019259977986f5912de5a6ee9471c760b9c08
Saurabh Kumar Tiwari <sky.royaltiwari@gmail.com>
2022-10-13 17:36:18 +0530

    p1-working

---
+++ contact-gallery/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactContainerComponent } from './contact-container/contact-container.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { IconComponent } from './header/icon/icon.component';
+import { SearchComponent } from './header/search/search.component';
+import { ContactCardComponent } from './contact-container/contact-card/contact-card.component';
+import { SidebarItemComponent } from './sidebar/sidebar-item/sidebar-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContactContainerComponent,
+    SidebarComponent,
+    IconComponent,
+    SearchComponent,
+    ContactCardComponent,
+    SidebarItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
